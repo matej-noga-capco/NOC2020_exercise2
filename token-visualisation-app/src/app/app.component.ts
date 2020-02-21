9f44a09d3b51b6fe47cac62414c46aa468efb4c5
Posch <Helmut.Posch@capco.com>
2020-02-21 10:17:09 +0100

    First prototype

--- token-visualisation-app/src/app/app.component.ts
+++ token-visualisation-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'token-visualisation-app';
+  backendUrl = 'http://localhost:4200/';
+  token = '';
+
+  i = 0;
+  postForm: FormGroup;
+
+  requestData: RequestTokenDto = new RequestTokenDto();
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    // form init
+    this.postForm = fb.group({
+      'config': fb.array([])
+    });
+    this.addConfigBlock();
+  }
+
+  addConfigBlock() {
+    const settingBlock = this.postForm.get('config') as FormArray;
+    settingBlock.push(this.fb.group({['key' + this.i] : '', ['value' + this.i]: '' }));
+    this.i++;
+  }
+
+  sendPostRequest(event: Event) {
+    debugger;
+    this.requestData.keyValuePairs = (this.postForm.get('config') as FormArray).value;
+    this.http.post(this.backendUrl, JSON.stringify(this.requestData)).toPromise().then((res: Response) => {
+      alert(JSON.stringify(res.json()));
+    });
+  }
+
+  sendGetRequest(event: Event) {
+    this.http.get(this.backendUrl).toPromise().then((res: Response) => {
+      alert(res.json());
+    });
+  }
+}
+
+class RequestTokenDto {
+  issuer: any = '';
+  subject: any = '';
+  datetime: any = '';
+  keyValuePairs: any = [];
 }
--- token-visualisation-app/src/app/app.module.ts
+++ token-visualisation-app/src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FooterComponent} from "./_modules/footer/footer.component";
+import {HeaderComponent} from "./_modules/header/header.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDatepicker, MatDatepickerModule} from "@angular/material/datepicker";
+import {DateAdapter, MatNativeDateModule, NativeDateAdapter} from "@angular/material/core";
+import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaterialTimepickerModule,
+    ReactiveFormsModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [HttpClient, NativeDateAdapter],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
